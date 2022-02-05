02ffc960e1358cdd1407c3130a843184a1a2d4bd
Federico Beffa <beffa@fbengineering.ch>
2022-02-05 22:05:04 +0100

    * Initial version.

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Some packages not yet available in nixpkgs";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    nixpkgs.lib.recursiveUpdate
+
+      (let
+        callPackageUnfree = pkg:
+          with import "${nixpkgs}" {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          }; callPackage pkg;
+      in rec {
+
+        packages.x86_64-linux.wolfram-engine =
+          callPackageUnfree ./pkgs/applications/science/math/wolfram-engine { };
+
+        wolfram-for-jupyter-kernel =
+          callPackageUnfree ./pkgs/applications/editors/jupyter-kernels/wolfram/default.nix { wolfram-engine = packages.x86_64-linux.wolfram-engine; };
+
+        packages.x86_64-linux.wolfram-notebook =
+          callPackageUnfree ./pkgs/applications/science/math/wolfram-engine/notebook.nix { inherit wolfram-for-jupyter-kernel; };
+
+      })
+
+      (flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in rec {
+
+          packages.trilinos =
+            pkgs.callPackage ./pkgs/development/libraries/science/math/trilinos { };
+
+          packages.trilinos-mpi =
+            pkgs.callPackage ./pkgs/development/libraries/science/math/trilinos { withMPI = true; };
+
+          packages.xschem =
+            pkgs.callPackage ./pkgs/applications/science/electronics/xschem { };
+
+          packages.xyce =
+            pkgs.callPackage ./pkgs/applications/science/electronics/xyce { trilinos = packages.trilinos; };
+
+          packages.xyce-parallel =
+            pkgs.callPackage ./pkgs/applications/science/electronics/xyce { withMPI = true; trilinos = packages.trilinos-mpi; };
+
+        }));
+
+}
