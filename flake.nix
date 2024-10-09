7d8953bc0e22b999b9b36464d0cf3b86cc5d9871
Federico Beffa <beffa@fbengineering.ch>
2024-10-09 17:29:03 +0200

    Remove julia wrappers as replaced by withPackages in nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Some packages not yet available in nixpkgs";
 
   inputs = {
-    # nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    # nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    # nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     # use the same revision as the one on my NixOS system found with
     # nixos-version --hash
-    # nixpkgs.url = github:NixOS/nixpkgs/15b75800dce80225b44f067c9012b09de37dfad2;
+    nixpkgs.url = github:NixOS/nixpkgs/944b2aea7f0a2d7c79f72468106bc5510cbf5101;
   };
 
   outputs = { self, nixpkgs }:
@@ -14,8 +14,6 @@
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-      wrapJulia = system: julia:
-          pkgs.${system}.callPackage ./pkgs/development/compilers/julia/build-fhs-env.nix { inherit julia; };
     in
     {
       packages = forAllSystems (system: rec {
@@ -32,21 +30,6 @@
 
         xschem = pkgs.${system}.callPackage ./pkgs/applications/science/electronics/xschem { };
         
-        # Julia
-        julia-lts = julia_16-bin;
-        julia-stable = julia_19;
-        julia = julia-stable;
-        julia-lts-bin = julia_16-bin;
-        julia-stable-bin = julia_19-bin;
-        julia-bin = julia-stable-bin;
-
-        julia_16-bin =  wrapJulia system pkgs.${system}.julia_16-bin;
-        julia_18-bin = wrapJulia system pkgs.${system}.julia_18-bin;
-        julia_19-bin = wrapJulia system pkgs.${system}.julia_19-bin;
-
-        julia_18 = wrapJulia system pkgs.${system}.julia_18;
-        julia_19 = wrapJulia system pkgs.${system}.julia_19;
-
       });
     };
 }
